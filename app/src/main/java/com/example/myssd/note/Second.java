1be7f9e04908a46fcb4d68d1f4094cf3c8c6c09f
Panasenko Vova <panvovasasha@gmail.com>
2018-07-12 23:41:26 +0300

    With pretty text inside SecondActivity(works finally)

--- app/src/main/java/com/example/myssd/note/Second.java
+++ app/src/main/java/com/example/myssd/note/Second.java
@@ -3,6 +3,7 @@ package com.example.myssd.note;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.os.Bundle;
@@ -17,6 +18,8 @@ import android.widget.Toast;
 
 public class Second extends AppCompatActivity {
     private Toolbar toolbar;
+    private EditText mTitleText;
+    private EditText mBodyText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +27,7 @@ public class Second extends AppCompatActivity {
         toolbar = findViewById(R.id.my_toolbar);
         // toolbar.setBackgroundColor(Color.parseColor("#80000000"));
         setSupportActionBar(toolbar);
+        mBodyText = (EditText) findViewById(R.id.body);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -33,7 +37,10 @@ public class Second extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.action_add:
+            case R.id.edit:
+                Toast.makeText(getApplicationContext(), "Everything is working fine ", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.share:
                 Toast.makeText(getApplicationContext(), "Everything is working fine ", Toast.LENGTH_SHORT).show();
                 return true;
 
@@ -54,21 +61,29 @@ public class Second extends AppCompatActivity {
             mRect = new Rect();
             mPaint = new Paint();
             mPaint.setStyle(Paint.Style.STROKE);
-            mPaint.setColor(0x80000000);
+            mPaint.setColor(Color.BLUE);
         }
 
         @Override
         protected void onDraw(Canvas canvas) {
-            int count = getLineCount();
+            int height = getHeight();
+            int line_height = getLineHeight();
+
+            int count = height / line_height;
+
+            if (getLineCount() > count)
+                count = getLineCount();
+
             Rect r = mRect;
             Paint paint = mPaint;
+            int baseline = getLineBounds(0, r);
 
             for (int i = 0; i < count; i++) {
-                int baseline = getLineBounds(i, r);
                 canvas.drawLine(r.left, baseline + 1, r.right, baseline + 1, paint);
+                baseline += getLineHeight();
+                super.onDraw(canvas);
             }
 
-            super.onDraw(canvas);
         }
     }
 }
